f73294f6da07c522ec95cbabf18d500f86ee5afb
Sardor Xoshimjonov <103178397+sxoshimjonov@users.noreply.github.com>
2022-04-21 13:15:15 +0500

    sfg

---
+++ app.js
@@ -0,0 +1,10 @@
+let icon = document.querySelector(".menuicun")
+let liks_ul = document.querySelector(".liks_ul")
+let span1 = document.querySelector("#span1")
+
+function addClass(){
+    // liks_ul.setAttribute("class","yangiClass" )
+    // console.log("asf");
+    span1.setAttribute("class", "yangiClass")
+    console.log("asdfasdf");
+}
